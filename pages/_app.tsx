2b4c31b6e511844538aac85bc6db5b03a3b72be4
kaznak <kaznak.at.work@gmail.com>
2020-09-27 06:48:01 +0900

    pages/_app.tsx: use chakra

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import { AppProps } from 'next/app'
 
+import theme from '../lib/theme'
+import { ChakraProvider } from '@chakra-ui/core'
+
 const App = (appProps: AppProps) => {
   const { Component, pageProps } = appProps
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default App
