0bce1e67292e2400d649ad0ba685919b663f89f2
kaznak <kaznak.at.work@gmail.com>
2020-09-27 22:50:19 +0900

    test/pages/chakra.test.tsx: add a test

---
+++ test/pages/chakra.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from '../testUtils'
+import Page from '../../pages/chakra'
+
+describe('Home page', () => {
+  it('matches snapshot', () => {
+    const { asFragment } = render(<Page />, {})
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
