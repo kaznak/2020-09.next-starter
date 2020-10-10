3aa0783c5e17e4e352ed98367861f77dd6f513f3
kaznak <kaznak.at.work@gmail.com>
2020-10-10 23:06:30 +0900

    pages/api/graphql.ts: fix a warning message

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,9 +1,15 @@
 import { ApolloServer } from 'apollo-server-express'
 import { typeDefs, resolvers } from '../../lib/schema'
 
+import { NextApiRequest, NextApiResponse } from 'next/types'
+
 const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
+}).getMiddleware({
+  path: '/api/graphql',
+  cors: false,
+  bodyParserConfig: true,
 })
 
 export const config = {
@@ -12,11 +18,21 @@ export const config = {
   },
 }
 
-/* !!TODO!! !!FIXME!! 
-  A warning message is repeated, that is
- ``API resolved without sending a response for /, this may result in stalled requests.``
- */
-export default apolloServer.getMiddleware({
-  path: '/api/graphql',
-  cors: false,
-})
+function useMiddleware(middleware: any) {
+  // !!TODO!! fix the type of middleware
+  return async (req: NextApiRequest, res: NextApiResponse) =>
+    new Promise<void>((resolve, reject) => {
+      middleware(req, res, (result: any) => {
+        // !!TODO!! fix the type of result
+        if (result instanceof Error) {
+          // !!TODO!! output error log
+          res.status(500)
+          return reject(result)
+        }
+        // !!TODO!! output access log
+        return resolve(result)
+      })
+    })
+}
+
+export default useMiddleware(apolloServer)
