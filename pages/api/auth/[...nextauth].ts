5bb4d7290a38a049817da4f05215a3a80744c29f
kaznak <kaznak.at.work@gmail.com>
2020-10-24 00:18:19 +0900

    pages/api/auth/[...nextauth].ts: add a auth api

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,20 @@
+import NextAuth, { InitOptions } from 'next-auth'
+import Providers from 'next-auth/providers'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+const options: InitOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+  ],
+
+  // A database is optional, but required to persist accounts in a database
+  database: process.env.DATABASE_URL,
+}
+
+export default (req: NextApiRequest, res: NextApiResponse) =>
+  NextAuth(req, res, options)
