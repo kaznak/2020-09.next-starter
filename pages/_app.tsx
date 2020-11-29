964f88ce3292a229361ba3122d0a781480d256ca
kaznak <kaznak.at.work@gmail.com>
2020-11-29 14:43:57 +0900

    wip

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
 import { AppProps } from 'next/app'
+import { SSRProvider, Provider, defaultTheme } from '@adobe/react-spectrum'
+
+import 'normalize.css'
 
 const App = (appProps: AppProps) => {
   const { Component, pageProps } = appProps
-  return <Component {...pageProps} />
+  const locale = 'ja-JP'
+  return (
+    <SSRProvider>
+      <Provider
+        theme={defaultTheme}
+        locale={locale}
+        minWidth="100vw"
+        minHeight="100vh"
+        position="absolute"
+        top={0}
+      >
+        <Component {...pageProps} />
+      </Provider>
+    </SSRProvider>
+  )
 }
 
 export default App
